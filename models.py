3fd1e6ca00f2f6e2efc380100960aec96c804321
JSchuurmans <jetzeschuurmans@gmail.com>
2019-08-28 08:51:44 +0000

    NaimishNet

--- models.py
+++ models.py
@@ -118,8 +118,145 @@ class NaimishNet(nn.Module):
         x = self.drop5(x)
         
         x = F.relu(self.fc2(x))
-        x = self.drop5(x)
+        x = self.drop6(x)
         
         x = self.fc3(x)
+        
+        return x
+    
+    
+    
+class NaimNet(nn.Module):
+    def __init__(self):
+        super(NaimNet, self).__init__()
+        
+        # (224-5)/1+1 = 220
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.drop1 = nn.Dropout(.1)
+        # 220/2 = 110
+        
+        # (110-4)/1 +1 = 107
+        self.conv2 = nn.Conv2d(32, 64, 4)
+        self.drop2 = nn.Dropout(.2)
+        # 107/2 = 53
+        
+        # (53-3)/1 +1 = 51
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.drop3 = nn.Dropout(.3)
+        # 51/2 = 25
+        
+        # (25-2)/1+1 = 24
+        self.conv4 = nn.Conv2d(128, 256, 2)
+        self.drop4 = nn.Dropout(.4)
+        # 24/2 = 12
+        
+#         self.conv4 = nn.Conv2d(256, 512 1)
+#         self.drop4 = nn.Dropout(.4)
+        
+        self.pool = nn.MaxPool2d(2,2)
+        
+        self.fc1 = nn.Linear(256*12*12, 1000)
+        self.drop5 = nn.Dropout(.5)
+        
+        self.fc2a = nn.Linear(1000, 1000)
+        self.drop6 = nn.Dropout(.6)
+        
+        self.fc2b = nn.Linear(1000, 1000)
+#         self.drop6 = nn.Dropout(.6)
+        
+        self.fc3 = nn.Linear(1000, 136)
+        
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.drop1(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.drop2(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.drop3(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.drop4(x)
+        
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.drop5(x)
+        
+        x = F.relu(self.fc2a(x))
+        x = self.drop6(x)
+        
+        x = F.relu(self.fc2b(x))
+        x = self.drop6(x)
+        
+        x = self.fc3(x)
+        
+        return x
+    
+    
+class NaNet(nn.Module):
+    def __init__(self):
+        super(NaNet, self).__init__()
+        
+        # (224-5)/1+1 = 220
+        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.drop1 = nn.Dropout(.1)
+        # 220/2 = 110
+        
+        # (110-4)/1 +1 = 107
+        self.conv2 = nn.Conv2d(32, 64, 4)
+        self.drop2 = nn.Dropout(.2)
+        # 107/2 = 53
+        
+        # (53-3)/1 +1 = 51
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.drop3 = nn.Dropout(.3)
+        # 51/2 = 25
+        
+        # (25-2)/1+1 = 24
+        self.conv4 = nn.Conv2d(128, 256, 2)
+        self.drop4 = nn.Dropout(.4)
+        # 24/2 = 12
+        
+        # (12-1)/1 +1 = 12
+        self.conv5 = nn.Conv2d(256, 512, 1)
+#         self.drop4 = nn.Dropout(.4)
+        # 12/2 = 6
+        
+        self.pool = nn.MaxPool2d(2,2)
+        
+        self.fc1 = nn.Linear(512*6*6, 3000)
+        self.drop5 = nn.Dropout(.5)
+        
+        self.fc2 = nn.Linear(3000, 2000)
+        self.drop6 = nn.Dropout(.6)
+        
+        self.fc3 = nn.Linear(2000, 1000)
+#         self.drop6 = nn.Dropout(.6)
+        
+        self.fc4 = nn.Linear(1000, 136)
+        
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.drop1(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.drop2(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.drop3(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.drop4(x)
+        x = self.pool(F.relu(self.conv5(x)))
+        x = self.drop4(x)
+        
+        x = x.view(x.size(0), -1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.drop5(x)
+        
+        x = F.relu(self.fc2(x))
+        x = self.drop6(x)
+        
+        x = F.relu(self.fc3(x))
+        x = self.drop6(x)
+        
+        x = self.fc4(x)
         
         return x
